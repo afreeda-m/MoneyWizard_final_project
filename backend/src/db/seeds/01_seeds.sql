f0cccf3184d6edb6e1295d0fcdcda851d4f4ae3a
Rora Alem <rora.goitom@gmail.com>
2023-11-15 08:43:37 -0500

    Add seeds and package json

--- backend/src/db/seeds/01_seeds.sql
+++ backend/src/db/seeds/01_seeds.sql
@@ -4,5 +4,13 @@ INSERT INTO users (name, email, password)
 VALUES ('Rora Alem', 'rora@email.com', '123');
 
 
-
-
+INSERT INTO transactions(user_id,category_id, account_id, amount, notes)
+VALUES('Shopping', 2, 200, 'Bought some clothes')
+INSERT INTO transactions(user_id,category_id, account_id, amount, notes)
+VALUES('Food', 1, 50, 'Groceries')
+INSERT INTO transactions(user_id,category_id, account_id, amount)
+VALUES('Dinning Out', 3, 100)
+INSERT INTO transactions(user_id,category_id, account_id, amount, notes)
+VALUES('Freelancing', 4, 5000, 'Recieved from client')
+INSERT INTO transactions(user_id,category_id, account_id, amount, notes)
+VALUES('Salary', 5, 1000, 'Monthly Income')
