3ce673171d2875096424b464a85f10deb82d149e
Rora Alem <rora.goitom@gmail.com>
2023-11-17 13:35:35 -0500

    Mount categoriesRouter on the root path

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const transactionsRouter = require('./routes/transactions.js')
+const categoriesRouter = require('./routes/categories.js')
 
 app.use(express.static('public'));
 
+// middleware set up to parse the JSON body
+app.use(express.json());
+
 app.use('/', transactionsRouter);
+app.use('/', categoriesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
