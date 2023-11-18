60050eb6869080ea9fca898e8512f80afe2a23a0
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-18 15:06:55 -0500

    create TransactionList component

---
+++ frontend/src/components/TransactionList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/TransactionList.css"
+
+import TransactionListItem from "./TransactionListItem";
+
+const TransactionList = (props) => {
+
+  const { transactions, categories, accounts, getCategoryIconById, getCategoryNameById, getAccountNameById } = props;
+
+  const listOfTransactions = transactions.map((transaction) => {
+    return (
+      <TransactionListItem
+        key={transaction.id}
+        id={transaction.id}
+        category_icon={getCategoryIconById(transaction.category_id, categories)}
+        category_name={getCategoryNameById(transaction.category_id, categories)}
+        account_name={getAccountNameById(transaction.account_id, accounts)}
+        amount={transaction.amount}
+        notes={transaction.notes}
+      />);
+  });
+
+  return (
+    <ul className="transaction-list">
+      {listOfTransactions}
+    </ul>
+  );
+};
+
+export default TransactionList;
