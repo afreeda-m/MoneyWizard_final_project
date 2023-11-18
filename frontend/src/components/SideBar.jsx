d9dd5584f5b1a85e50538adf3e8fb7d19b97ffdc
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-17 21:45:43 -0500

    create SideBar component

---
+++ frontend/src/components/SideBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="side-bar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Transaction</li>
+        <li>Reports</li>
+        <li>Budgets</li>
+        <li>Recurring Transactions</li>
+        <li>Categories Management</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
