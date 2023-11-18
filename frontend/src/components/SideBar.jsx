ffe9f1ac1b321d16645a2fba8bc12fa19d597b2a
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-18 01:03:08 -0500

    create SideBar component

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { SideBarData } from "../data/SideBarData";
+import '../styles/SideBar.css';
 
 const SideBar = () => {
+
   return (
-    <div className="side-bar">
-      <ul>
-        <li>Dashboard</li>
-        <li>Transaction</li>
-        <li>Reports</li>
-        <li>Budgets</li>
-        <li>Recurring Transactions</li>
-        <li>Categories Management</li>
-      </ul>
-    </div>
+    <>
+        <nav className={'side-bar'}>
+          <img id="logo" src="MWlogo.png" alt="Money Wizard logo"></img>
+          <ul className='side-bar-items-list'>
+            {SideBarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+    </>
   );
 };
 
