2cd24096fcfd9f96aebdae30b13022f035ec6d2a
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-18 15:32:44 -0500

    add FloatingButton to Transactions component

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -5,13 +5,18 @@ import accounts from "../mocks/accounts";
 import { getAccountNameById, getCategoryIconById, getCategoryNameById } from "../helpers/mockhelpers";
 import TransactionList from "../components/TransactionList";
 import "../styles/Transactions.css"
+import FloatingButton from "../components/FloatingButton";
 
 
 
 const Transactions = () => {
   return (
     <div className="transactions">
+
       <h1>List of transactions</h1>
+
+      <h3>Placeholder for filter</h3>
+
       <TransactionList
       transactions={transactions}
       categories={categories}
@@ -20,6 +25,8 @@ const Transactions = () => {
       getCategoryIconById={getCategoryIconById}
       getCategoryNameById={getCategoryNameById}
       />
+
+      <FloatingButton />
 
     </div>
   );
