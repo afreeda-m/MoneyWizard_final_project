a4dd055b7ddebb13c9ff6e6eb76cb762dcfce7a8
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-18 15:06:39 -0500

    edit linting

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -7,21 +7,21 @@ const SideBar = () => {
 
   return (
     <>
-        <nav className={'side-bar'}>
-          <img id="logo" src="MWlogo.png" alt="Money Wizard logo"></img>
-          <ul className='side-bar-items-list'>
-            {SideBarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+      <nav className='side-bar'>
+        <img id="logo" src="MWlogo.png" alt="Money Wizard logo"></img>
+        <ul className='side-bar-items-list'>
+          {SideBarData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </>
   );
 };
