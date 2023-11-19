34607ec40432bfb2917f80034431868806a10b60
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-19 11:25:51 -0500

    adding styles and components to Accounts page

--- frontend/src/pages/Accounts.jsx
+++ frontend/src/pages/Accounts.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import accounts from "../mocks/accounts";
+import AccountList from "../components/AccountList";
+import "../styles/Accounts.css";
+import FloatingButton from "../components/FloatingButton";
 
 const Accounts = () => {
   return (
     <div className="accounts">
-      <h1>Accounts Page</h1>
+      <h1>List of accounts</h1>
+
+      <div>Place holder for Pie chart and  total balance</div>
+
+      <AccountList
+        accounts={accounts}
+      />
+
+    <FloatingButton />
+
     </div>
-  )
-}
+  );
+};
 
 export default Accounts;
