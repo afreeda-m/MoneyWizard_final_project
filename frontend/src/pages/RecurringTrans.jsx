4326610dc9a953d6aa1c6e544687dbf55a798d46
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-19 11:24:05 -0500

    add initial styling

--- frontend/src/pages/RecurringTrans.jsx
+++ frontend/src/pages/RecurringTrans.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/RecurringTrans.css"
 
 const RecurringTransactions = () => {
   return (
--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/Reports.css"
 
 const Report = () => {
   return (
-    <div className="report">
+    <div className="reports">
       <h1>Report Page</h1>
     </div>
   );
