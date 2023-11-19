9b806df01fa29f7ef9b758fe7b89ddd522ce1af4
Rora Alem <rora.goitom@gmail.com>
2023-11-19 09:54:57 -0500

    Revert "Fetch categories"
    This reverts commit 193021eea7a41c0c9acd15d3ae208e616964a105.
    
    Revert changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-// In a React component
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Datepicker from './Datepicker';
+
 function App() {
-
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    // Fetch categories when the component mounts
-    axios.get('http://localhost:8080/categories')
-      .then(response => {
-        setCategories(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching categories:', error);
-      });
-  }, []); // Empty dependency array ensures the effect runs only once
-
   return (
     <div className="App">
-      <h2>Categories List</h2>
-      <ul>
-        {categories.map(category => (
-          <li key={category.id}>{category.category_name}</li>
-        ))}
-      </ul>
-
-      <Datepicker/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
-
+}
 
 export default App;
