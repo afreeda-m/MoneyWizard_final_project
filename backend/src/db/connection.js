2d9162b5d42a5d1e3b4b5b36023afd88e1d0bb0d
Rora Alem <rora.goitom@gmail.com>
2023-11-20 07:36:45 -0500

    Add password for db

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -5,6 +5,7 @@ const client = new pg.Client({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 });
 
