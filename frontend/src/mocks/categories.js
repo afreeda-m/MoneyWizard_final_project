682b9bceec98d1acf7b2b57bafa64b4a699b8ea5
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-20 21:17:17 -0500

    update categories mock data

--- frontend/src/mocks/categories.js
+++ frontend/src/mocks/categories.js
@@ -3,70 +3,70 @@ const categories = [
     id: 1,
     category_name: "Shopping",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 2,
     category_name: "Dining",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 3,
     category_name: "Entertainment",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 4,
     category_name: "Utilities",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 5,
     category_name: "Transportation",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 6,
     category_name: "Healthcare",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 7,
     category_name: "Education",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 8,
     category_name: "Gifts",
     type: "Expense",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 9,
     category_name: "Salary",
     type: "Income",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   },
   {
     id: 10,
     category_name: "Freelance",
     type: "Income",
-    logo_url: "sample",
+    logo_url: "sample_logo",
     user_id: 1
   }
 ];
