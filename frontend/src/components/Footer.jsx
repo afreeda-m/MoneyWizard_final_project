b6cbb451f077f2b9247ecd3c31b24709b80752ec
jvarghe <vcs.inbox@outlook.com>
2023-11-21 21:02:59 -0500

    Add the 'Footer.jsx' component. Import it into 'Home.jsx'.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+
+function Footer() {
+
+  return (
+
+    // `min-vw-100`: Set to 100% of viewport.
+    // `bg-body-tertiary`: Give its the same colour as the NavBar.
+    <footer className='navbar-expand navbar-light bg-body-tertiary min-vw-100'>
+
+      <Container>
+
+        {/* If you want to auto-generate and insert the current year for the
+          * copyright, use this snippet:
+          *
+          *   {new Date().getFullYear()}
+          *
+          */}
+        <div className='text-center py-3'>
+
+          <p> Copyright &copy; 2023: &nbsp;
+
+            <a className='text-dark' href='https://github.com/afreeda-m/MoneyWizard_final_project'>
+              Wizards of Money Wizard
+            </a>
+
+          </p>
+
+          <p>All Rights Reserved</p>
+
+        </div>
+
+      </Container>
+
+    </footer>
+
+  );
+
+}
+
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 
 
@@ -9,6 +11,25 @@ function Home() {
     <div className="home">
 
       <NavBar />
+
+      {/* For this page, I want the `Footer` component to hug the bottom of the
+        * page, so I need to make the `main` element take 85% of the viewport
+        * width.
+        *
+        * However, Bootstrap has defined `min-vh-100` only, but no other value.
+        * So if want `min-vh-80`, you're going to have to write custom CSS.
+        *
+        * Because this project is intended to use minimum custom CSS, I've
+        * chosen to add an inline style here. To use inline styling with JSX,
+        * use this format: `style={{camelCaseAttribute: 'string'}}`.
+        */}
+      <main className='text-center' style={{minHeight: "85vh"}}>
+
+        <h1>Welcome to Money Wizard!</h1>
+
+      </main>
+
+      <Footer />
 
     </div>
 
