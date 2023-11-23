9173c26ed1cbb9d5cc89f421e8c0364b4c62e3ad
jvarghe <vcs.inbox@outlook.com>
2023-11-23 09:23:18 -0500

    Replace icons from 'react-icons' with Material UI icons in 'SideBarData.js'.

--- frontend/src/data/SideBarData.js
+++ frontend/src/data/SideBarData.js
@@ -1,49 +1,54 @@
+import CategoryIcon from '@mui/icons-material/Category';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PieChartIcon from '@mui/icons-material/PieChart';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import SavingsIcon from '@mui/icons-material/Savings';
 import React from "react";
-import * as IoIcons from "react-icons/io5";
-import * as MdIcons from "react-icons/md";
 
 
 export const SideBarData = [
   {
     title: 'Dashboard',
     path: '/dashboard',
-    icon: <MdIcons.MdDashboard />,
+    icon: <DashboardIcon />,
     cName: 'side-bar-item'
   },
   {
     title: 'Transactions',
     path: '/transactions',
-    icon: <MdIcons.MdOutlineAttachMoney  />,
+    icon: <PointOfSaleIcon />,
     cName: 'side-bar-item'
   },
   {
     title: 'Accounts',
     path: '/accounts',
-    icon: <MdIcons.MdAccountBalanceWallet />,
+    icon: <SavingsIcon />,
     cName: 'side-bar-item'
   },
   {
     title: 'Reports',
     path: '/reports',
-    icon: <IoIcons.IoDocumentsSharp />,
+    icon: <RequestQuoteIcon />,
     cName: 'side-bar-item'
   },
   {
     title: 'Budgets',
     path: '/budgets',
-    icon: <IoIcons.IoPieChart />,
+    icon: <PieChartIcon />,
     cName: 'side-bar-item'
   },
   {
     title: 'Recurring Transactions',
     path: '/recurringtransactions',
-    icon: <MdIcons.MdOutlineRepeat />,
+    icon: <CurrencyExchangeIcon />,
     cName: 'side-bar-item'
   },
   {
     title: 'Categories Management',
     path: '/categoriesmanagement',
-    icon: <MdIcons.MdCategory />,
+    icon: <CategoryIcon />,
     cName: 'side-bar-item'
   },
 ];
