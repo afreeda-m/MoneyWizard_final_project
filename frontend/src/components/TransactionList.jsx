a83989dc033b6057c81a2ee8534164ef319f1f0a
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-24 01:35:52 -0500

    Create transaction list using React-bootstrap

--- frontend/src/components/TransactionList.jsx
+++ frontend/src/components/TransactionList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import "../styles/TransactionList.scss";
+// import "../styles/TransactionList.scss";
 
-import TransactionListItem from "./TransactionListItem";
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
 
 const TransactionList = (props) => {
 
@@ -9,21 +17,54 @@ const TransactionList = (props) => {
 
   const listOfTransactions = transactions.map((transaction) => {
     return (
-      <TransactionListItem
-        key={transaction.id}
-        id={transaction.id}
-        category_icon={getCategoryIconById(transaction.category_id, categories)}
-        category_name={getCategoryNameById(transaction.category_id, categories)}
-        account_name={getAccountNameById(transaction.account_id, accounts)}
-        amount={transaction.amount}
-        notes={transaction.notes}
-      />);
+      <ListGroupItem style={{innerWidth: "1000px"}}>
+        <Container>
+          <Row >
+
+            <Col sm={2}>
+              <Image src="bank.png" roundedCircle />
+            </Col>
+
+            <Col lg={3}>
+              <div className="category_name">
+                {getCategoryNameById(transaction.category_id, categories)}
+              </div>
+              <div className="account_name">
+                {getAccountNameById(transaction.account_id, accounts)}
+              </div>
+            </Col>
+
+            <Col lg={3}>
+              <div className="transaction_notes">
+                {transaction.notes}
+              </div>
+            </Col>
+
+            <Col sm={2}>
+              <div className="transaction_amount">
+                {transaction.amount}
+              </div>
+              <div className="transaction_date">
+                <i>1111/11/11</i>
+              </div>
+            </Col>
+
+            <Col sm={1}>
+              <MoreVertIcon />
+            </Col>
+
+          </Row>
+        </Container>
+      </ListGroupItem>
+    );
   });
 
   return (
-    <ul className="transaction-list">
+
+    <ListGroup >
       {listOfTransactions}
-    </ul>
+    </ListGroup>
+
   );
 };
 
