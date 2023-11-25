bb9a45ff3df18ce8b6a1cb54213ab5159921fd75
Rora Alem <rora.goitom@gmail.com>
2023-11-25 07:17:39 -0500

    Render Income and expenses pie chart

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -4,14 +4,29 @@ import ColumnChart from "../components/ColumnChart";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import monthlyData from '../mocks/monthlyData'; // Importing mock data
-
+import IncomePieChart from "../components/IncomePieChart";
+import ExpensePieChart from "../components/ExpensePieChart";
+import { calculateIncomeDistribution, calculateExpenseDistribution } from "../helpers/mockhelpers";
 
 const Report = () => {
+
+// Calculate income distribution by category
+const incomeDistributionData = calculateIncomeDistribution();
+const expenseDistributionData = calculateExpenseDistribution();
   return (
     <Container>
       <Row >
         <Col>
-          <ColumnChart monthlyData={monthlyData} />
+          <ColumnChart monthlyData={monthlyData}/>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col>
+          <IncomePieChart
+           incomeData={incomeDistributionData}/>
+        </Col>
+        <Col >
+        <ExpensePieChart expenseData={expenseDistributionData}/>
         </Col>
       </Row>
     </Container>
