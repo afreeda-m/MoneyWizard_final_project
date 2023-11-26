f21cf95fa94eb00509647fb47faa5d8d7dc15cb1
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-26 17:31:34 -0500

    add FilterBar to Transactions page

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import FloatingActionButton from "../components/FloatingActionButton";
 import TransactionList from "../components/TransactionList";
 import { getAccountNameById, getCategoryIconById, getCategoryNameById } from "../helpers/mockhelpers";
 import accounts from "../mocks/accounts";
 import categories from "../mocks/categories";
 import transactions from "../mocks/transactions";
+import FilterBar from "../components/FilterBar";
+import moment from 'moment';
 
 const Transactions = () => {
+
+  const [date, setDate] = useState(moment().format("MMMM YYYY"));
+
   return (
     <div className="d-flex flex-column align-items-center" >
 
       <h1>List of Transactions</h1>
 
-      <h3>Placeholder for filter</h3>
+      <FilterBar
+        date={date}
+        setDate={setDate}
+      />
 
       <TransactionList
         transactions={transactions}
