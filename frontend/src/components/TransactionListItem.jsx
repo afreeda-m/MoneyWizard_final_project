04c09d5ce73e66e35d07e025e74f1c2b2e8895ca
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-26 15:33:02 -0500

    move codes to TransactionListItem and pass down data as props

--- frontend/src/components/TransactionListItem.jsx
+++ frontend/src/components/TransactionListItem.jsx
@@ -1,32 +1,78 @@
 import React from "react";
 import "../styles/TransactionListItem.scss";
 
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import Dropdown from 'react-bootstrap/Dropdown';
+
 const TransactionListItem = (props) => {
 
-  const { id, category_icon, category_name, account_name, amount, notes } = props;
+  const { categoryIcon, categoryName, accountName, amount, notes, date } = props;
   return (
-    <div key={id} className="transaction-list-item">
+    <ListGroupItem className="shadow p-1 mb-1 rounded" style={{ width: "50vw" }} >
+      <Container>
 
-      <div className="category-icon">
-        {category_icon}
-      </div>
+        <Row className="d-flex align-items-center">
 
-      <div className="transaction-list-item-detail">
+          <Col className="d-flex justify-content-center" xs={2} >
+            <Image src={categoryIcon} />
+          </Col>
 
-        {category_name}
-        {account_name}
+          <Col xs={3} >
+            <div className="category_name">
+              <b>
+                {categoryName}
+              </b>
+            </div>
+            <div className="account_name">
+              <i>
+                {accountName}
+              </i>
+            </div>
+          </Col>
 
-        <div className="transaction-notes">
-          {notes}
-        </div>
+          <Col xs={4} >
+            <div className="transaction_notes">
+              {notes}
+            </div>
+          </Col>
 
-      </div>
+          <Col xs={2} className="d-flex flex-column align-items-end" >
+            <div className="transaction_amount">
+              {amount}
+            </div>
+            <div className="transaction_date">
+              <i>{date}</i>
+            </div>
+          </Col >
 
-      <div className="transaction-list-item-amount">
-        {amount}
-      </div>
+          {/* 3 dots icon with dropdown list show the Edit and Delete feature for a transaction */}
+          <Col xs={1} className="d-flex justify-content-end"  >
 
-    </div>
+            <Dropdown >
+              <Dropdown.Toggle variant="none" id="dropdown-basic">
+
+                <MoreVertIcon />
+
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+
+                <Dropdown.Item href="#">Edit</Dropdown.Item>
+                <Dropdown.Item href="#">Delete</Dropdown.Item>
+
+              </Dropdown.Menu>
+
+            </Dropdown>
+          </Col>
+
+        </Row>
+
+      </Container>
+    </ListGroupItem>
   );
 };
 
