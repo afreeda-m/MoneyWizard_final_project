538f0a0d9f99ce611ebf6db4dea0ecde3e11f1d6
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-26 15:32:39 -0500

    move codes to TransactionListItems and pass down data as props

--- frontend/src/components/TransactionList.jsx
+++ frontend/src/components/TransactionList.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 // import "../styles/TransactionList.scss";
-
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import TransactionListItem from "./TransactionListItem";
 
 
 const TransactionList = (props) => {
@@ -17,51 +11,16 @@ const TransactionList = (props) => {
   const listOfTransactions = transactions.map((transaction) => {
     return (
 
-      // Set the `TransactionList` component to be 50% of the viewport width.
-      <div style={{ width: "50vw" }}>
+      <TransactionListItem
+        key={transaction.id}
+        categoryIcon={"bank.png"}
+        categoryName={getCategoryNameById(transaction.category_id, categories)}
+        accountName={getAccountNameById(transaction.account_id, accounts)}
+        notes={transaction.notes}
+        amount={transaction.amount}
+        date={"1111/11/11"}
+      />
 
-        <ListGroupItem >
-          <Container>
-            <Row className="d-flex align-items-center">
-
-              <Col sm={2}>
-                <Image src="bank.png" roundedCircle />
-              </Col>
-
-              <Col lg={3}>
-                <div className="category_name">
-                  <b>
-                    {getCategoryNameById(transaction.category_id, categories)}
-                  </b>
-                </div>
-                <div className="account_name">
-                  {getAccountNameById(transaction.account_id, accounts)}
-                </div>
-              </Col>
-
-              <Col lg={3}>
-                <div className="transaction_notes">
-                  {transaction.notes}
-                </div>
-              </Col>
-
-              <Col sm={2}>
-                <div className="transaction_amount">
-                  {transaction.amount}
-                </div>
-                <div className="transaction_date">
-                  <i>1111/11/11</i>
-                </div>
-              </Col>
-
-              <Col className="d-flex" style={{ border: "1px solid black" }}>
-                <MoreVertIcon className="alignItems: flex-end" />
-              </Col>
-
-            </Row>
-          </Container>
-        </ListGroupItem>
-      </div>
     );
   });
 
