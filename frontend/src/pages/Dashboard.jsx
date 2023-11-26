8c708bc7928939b20c60505fdcb3c3ed882163d8
jvarghe <vcs.inbox@outlook.com>
2023-11-26 01:46:04 -0500

    Modify 'Dashboard.jsx' so that it displays its elements without issue.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <div className="dashboard d-flex flex-column align-items-center">
 
       <h1>Dashboard Page</h1>
 
