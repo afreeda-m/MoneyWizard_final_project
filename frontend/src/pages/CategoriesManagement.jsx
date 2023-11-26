611045ed1c88d120204ed6efc8e9703f99df723b
jvarghe <vcs.inbox@outlook.com>
2023-11-26 01:57:08 -0500

    Style the 'Categories Managment' page to with a basic style.

--- frontend/src/pages/CategoriesManagement.jsx
+++ frontend/src/pages/CategoriesManagement.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CategoriesManagement = () => {
   return (
-    <div className="catManagement">
+    <div className="categoriesManagement  d-flex flex-column align-items-center">
 
       <h1>Categories Management Page</h1>
 
