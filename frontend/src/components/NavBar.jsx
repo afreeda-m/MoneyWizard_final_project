daae6e0b225a58a2443f0c2d1798fa18c4bbc4c8
jvarghe <vcs.inbox@outlook.com>
2023-11-26 01:43:36 -0500

    Style 'Navbar.jsx' and 'Footer.jsx' so that they sit cleanly on all pages.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,54 +9,54 @@ import { FaBullseye } from 'react-icons/fa';
 function NavBar() {
   return (
 
-    <Navbar className="fluid-container bg-success" fixed='top'>
-      <Container>
+    <Navbar className="bg-success d-flex justify-content-center align-items-between px-4">
 
-        {/* BRAND NAME */}
-        <Navbar.Brand href="#home" className='text-light'>
+      {/* BRAND NAME */}
+      <Navbar.Brand href="#home" className='text-light'>
 
-        <FaBullseye size={45} className="mb-2 text-white" style={{marginRight: "1rem"}} />
-          Money Wizard</Navbar.Brand>
-        <Navbar.Toggle />
-
-
-        {/* THE FORM PORTION OF THE NAVBAR */}
-        <Navbar.Collapse className="justify-content-end">
-
-          {/* LOGIN FORM */}
-          <Form inline>
-            <InputGroup>
-
-              {/* Username Field */}
-              <Form.Control
-                name="username"
-                placeholder="Username"
-                aria-label="Username"
-                aria-describedby="Username Field"
-              />
-
-              {/* Password Field */}
-              <Form.Control
-                name="password"
-                placeholder="Password"
-                aria-label="Password"
-                aria-describedby="Password Field"
-              />
-
-              <Button type="submit">Login</Button>
-
-            </InputGroup>
-          </Form>
+        <FaBullseye size={45} className="mb-2 text-white" style={{ marginRight: "1rem" }} />
+        Money Wizard</Navbar.Brand>
+      <Navbar.Toggle />
 
 
-          {/* CODE TO DISPLAY SIGNED IN USER */}
-          {/* <Navbar.Text>
-              Signed in as: <a href="#login">Mark Otto</a>
-            </Navbar.Text> */}
+      {/* THE FORM PORTION OF THE NAVBAR */}
+      <Navbar.Collapse className="justify-content-end">
 
-        </Navbar.Collapse>
+        {/* LOGIN FORM */}
+        <Form inline>
+          <InputGroup>
 
-      </Container>
+            {/* Username Field */}
+            <Form.Control
+              name="username"
+              placeholder="Username"
+              aria-label="Username"
+              aria-describedby="Username Field"
+            />
+
+            {/* Password Field */}
+            <Form.Control
+              name="password"
+              placeholder="Password"
+              aria-label="Password"
+              aria-describedby="Password Field"
+            />
+
+
+            {/* Submit Button */}
+            <Button type="submit">Login</Button>
+
+          </InputGroup>
+        </Form>
+
+
+      {/* CODE TO DISPLAY SIGNED IN USER */}
+      {/* <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text> */}
+
+      </Navbar.Collapse>
+
     </Navbar>
 
   );
