a45fa3074328b6ee0e15b805e80448998f146f50
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-26 17:30:22 -0500

    add date to mock data

--- frontend/src/mocks/transactions.js
+++ frontend/src/mocks/transactions.js
@@ -6,7 +6,8 @@ const transactions = [
     category_id: 2,
     account_id: 1,
     amount: 200,
-    notes: "random note"
+    notes: "random note",
+    date: "2022-11-01"
   },
   {
     id: 2,
@@ -14,7 +15,8 @@ const transactions = [
     category_id: 5,
     account_id: 2,
     amount: 50,
-    notes: "shopping at mall"
+    notes: "shopping at mall",
+    date: "2022-10-02"
   },
   {
     id: 3,
@@ -22,7 +24,8 @@ const transactions = [
     category_id: 8,
     account_id: 3,
     amount: 120,
-    notes: "dinner with friends"
+    notes: "dinner with friends",
+    date: "2022-11-03"
   },
   {
     id: 4,
@@ -30,7 +33,8 @@ const transactions = [
     category_id: 3,
     account_id: 1,
     amount: 80,
-    notes: "movie night"
+    notes: "movie night",
+    date: "2022-11-03"
   },
   {
     id: 5,
@@ -38,7 +42,8 @@ const transactions = [
     category_id: 6,
     account_id: 2,
     amount: 30,
-    notes: "medicine purchase"
+    notes: "medicine purchase",
+    date: "2022-10-05"
   },
   {
     id: 6,
@@ -46,7 +51,8 @@ const transactions = [
     category_id: 1,
     account_id: 3,
     amount: 150,
-    notes: "grocery shopping"
+    notes: "grocery shopping",
+    date: "2022-11-06"
   },
   {
     id: 7,
@@ -54,7 +60,8 @@ const transactions = [
     category_id: 4,
     account_id: 1,
     amount: 100,
-    notes: "electricity bill"
+    notes: "electricity bill",
+    date: "2022-12-15"
   },
   {
     id: 8,
@@ -62,7 +69,8 @@ const transactions = [
     category_id: 7,
     account_id: 2,
     amount: 75,
-    notes: "textbooks for school"
+    notes: "textbooks for school",
+    date: "2022-11-12"
   },
   {
     id: 9,
@@ -70,7 +78,8 @@ const transactions = [
     category_id: 9,
     account_id: 3,
     amount: 300,
-    notes: "birthday gift"
+    notes: "birthday gift",
+    date: "2022-12-19"
   },
   {
     id: 10,
@@ -78,7 +87,8 @@ const transactions = [
     category_id: 10,
     account_id: 1,
     amount: 500,
-    notes: "monthly salary"
+    notes: "monthly salary",
+    date: "2022-11-24"
   },
   {
     id: 11,
@@ -86,7 +96,8 @@ const transactions = [
     category_id: 2,
     account_id: 2,
     amount: 40,
-    notes: "online shopping"
+    notes: "online shopping",
+    date: "2022-11-19"
   },
   {
     id: 12,
@@ -94,7 +105,8 @@ const transactions = [
     category_id: 5,
     account_id: 3,
     amount: 90,
-    notes: "lunch with colleagues"
+    notes: "lunch with colleagues",
+    date: "2022-11-22"
   },
   {
     id: 13,
@@ -102,7 +114,8 @@ const transactions = [
     category_id: 8,
     account_id: 1,
     amount: 75,
-    notes: "coffee meetup"
+    notes: "coffee meetup",
+    date: "2022-11-27"
   },
   {
     id: 14,
@@ -110,7 +123,8 @@ const transactions = [
     category_id: 3,
     account_id: 2,
     amount: 60,
-    notes: "concert tickets"
+    notes: "concert tickets",
+    date: "2022-11-30"
   },
   {
     id: 15,
@@ -118,7 +132,8 @@ const transactions = [
     category_id: 6,
     account_id: 3,
     amount: 25,
-    notes: "pharmacy visit"
+    notes: "pharmacy visit",
+    date: "2022-11-01"
   },
   {
     id: 16,
@@ -126,7 +141,8 @@ const transactions = [
     category_id: 1,
     account_id: 1,
     amount: 180,
-    notes: "weekly groceries"
+    notes: "weekly groceries",
+    date: "2022-11-13"
   },
   {
     id: 17,
@@ -134,7 +150,8 @@ const transactions = [
     category_id: 4,
     account_id: 2,
     amount: 120,
-    notes: "water bill"
+    notes: "water bill",
+    date: "2022-11-29"
   },
   {
     id: 18,
@@ -142,7 +159,8 @@ const transactions = [
     category_id: 7,
     account_id: 3,
     amount: 50,
-    notes: "art supplies"
+    notes: "art supplies",
+    date: "2022-11-26"
   },
   {
     id: 19,
@@ -150,7 +168,8 @@ const transactions = [
     category_id: 9,
     account_id: 1,
     amount: 200,
-    notes: "holiday gift"
+    notes: "holiday gift",
+    date: "2022-11-20"
   },
   {
     id: 20,
@@ -158,7 +177,8 @@ const transactions = [
     category_id: 10,
     account_id: 2,
     amount: 600,
-    notes: "bonus payment"
+    notes: "bonus payment",
+    date: "2022-11-24"
   }
 ];
 
