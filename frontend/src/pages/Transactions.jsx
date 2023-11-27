33d3e0f821d01f3b6e498c7f4b906ce492fc848b
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-27 16:02:05 -0500

    pulling data from backend

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import FloatingActionButton from "../components/FloatingActionButton";
 import TransactionList from "../components/TransactionList";
 import { getAccountNameById, getCategoryIconById, getCategoryNameById, getCategoryTypeById } from "../helpers/mockhelpers";
@@ -9,10 +10,52 @@ import FilterBar from "../components/FilterBar";
 import moment from 'moment';
 import TransactionModal from "../components/TransactionModal";
 
+
+
 const Transactions = () => {
+
+  const [accountsData, setAccountsData] = useState([]);
+  const [transactionsData, setTransactionsData] = useState([]);
+  const [categoriesData, setCategoriesData] = useState([]);
 
   const [date, setDate] = useState(moment().format("MMMM YYYY"));
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    // Fetch transactions when the component mounts
+    axios.get('/transactions')
+      .then((response) => {
+        setTransactionsData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching transactions', error);
+      });
+
+    // Fetch accounts data when the component mounts
+    axios.get('/accounts')
+      .then((response) => {
+        setAccountsData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching accounts', error);
+      });
+
+    // Fetch categories when the component mounts
+    axios.get('/categories')
+      .then(response => {
+        console.log(response.data);
+        setCategoriesData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching categories:', error);
+      });
+
+    // Empty dependency array ensures the effect runs only once
+  }, []);
+
+  console.log('Transactions data from backend', transactionsData);
+  console.log('Accounts data from backend', accountsData);
+  console.log('Categories data from backend', categoriesData);
 
   const toggleModal = () => setIsModalOpen(!isModalOpen);
 
