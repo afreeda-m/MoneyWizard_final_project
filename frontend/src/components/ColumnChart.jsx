58828aae08282db9d2811a07dc35a0edb943c5a5
Rora Alem <rora.goitom@gmail.com>
2023-11-28 14:50:27 -0500

    Modify Barchart using reCharts library

--- frontend/src/components/ColumnChart.jsx
+++ frontend/src/components/ColumnChart.jsx
@@ -1,59 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import CanvasJSReact from '@canvasjs/react-charts';
-import axios from 'axios';
 
-const { CanvasJSChart } = CanvasJSReact;
+const ColumnChart = ({ totalIncome, totalExpense }) => {
 
-const ColumnChart = ({incomeData, expenseData, date}) => {
-
-  const chartOptions = {
-    animationEnabled: true,
-    title: {
-      text: `Income and Expenses for ${date}`,
-    },
-    axisY: {
-      title: 'Amount (CAD)',
-    },
-    axisX: {
-      title: 'Categories Type',
-    },
-    legend: {
-      cursor: 'pointer',
-      itemclick: () => {},
-    },
-    data: [
-      {
-        type: 'column',
-        name: 'Income',
-        showInLegend: true,
-        dataPoints: incomeData.map(item => ({ label: item.label, y: item.income })),
-      },
-      {
-        type: 'column',
-        name: 'Expenses',
-        showInLegend: true,
-        dataPoints: expenseData.map(item => ({ label: item.label, y: item.expenses })),
-      },
-    ],
-  };
-
+  // Define the data points for the bar chart, with separate values for "income" and "expense"
+  const data = [
+    { category: 'income', income: totalIncome, expense: 0 }, // Data point for income
+    { category: 'expense', income: 0, expense: totalExpense }, // Data point for expense
+  ];
 
   return (
     <Container className="mt-5">
       <Row className="justify-content-md-center">
         <Col md={{ span: 6, offset: 3 }}>
-          {/* Display the dropdown for selecting the month */}
-
+          {/* Display the dropdown for selecting the month (not implemented in the provided code) */}
         </Col>
       </Row>
       <Row className="mt-3">
         <Col md={{ span: 6, offset: 3 }}>
           {/* Display the bar chart with shadow and border */}
           <div className="shadow p-3 mb-5 bg-white rounded" style={{ border: '1px solid #ccc', minHeight: '400px' }}>
-            <CanvasJSChart options={chartOptions} />
+            {/* Create a responsive container for the bar chart */}
+            <ResponsiveContainer width="100%" height="100%">
+              {/* Define the BarChart component with data and margin */}
+              <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+                {/* Define the X-axis with the dataKey as "category" */}
+                <XAxis dataKey="category" />
+                {/* Define the Y-axis */}
+                <YAxis />
+                {/* Enable tooltip for displaying data */}
+                <Tooltip />
+                {/* Enable legend for displaying data series */}
+                <Legend />
+                {/* Create a bar for "income" with a unique color */}
+                <Bar dataKey="income" stackId="1" fill="#8884d8" />
+                {/* Create a bar for "expense" with a unique color */}
+                <Bar dataKey="expense" stackId="1" fill="#82ca9d" />
+              </BarChart>
+            </ResponsiveContainer>
           </div>
         </Col>
       </Row>
