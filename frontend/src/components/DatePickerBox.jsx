bc5eb3e1075d70f24b31e7ee49cead667e74ddcb
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-28 22:06:19 -0500

    pass chosenTransaction props to DatePickerBox to display data for editing transaction/transfer

--- frontend/src/components/DatePickerBox.jsx
+++ frontend/src/components/DatePickerBox.jsx
@@ -1,14 +1,15 @@
 import { DatePicker } from "@mui/x-date-pickers";
+import moment from "moment";
 
 const DatePickerBox = (props) => {
 
-  const { transactionDate, pickTransactionDate } = props;
+  const { transactionDate, pickTransactionDate, chosenTransaction } = props;
 
   return (
 
     <DatePicker
       sx={{ width: "50%" }}
-      value={transactionDate}
+      value={chosenTransaction ? moment(chosenTransaction.transaction_date) : transactionDate}
       onChange={(newDate) => {
         pickTransactionDate(newDate);
       }}
