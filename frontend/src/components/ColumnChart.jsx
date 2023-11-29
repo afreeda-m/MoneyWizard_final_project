96e0114ef7031d33ce2708187c8801bf772bafe0
Rora Alem <rora.goitom@gmail.com>
2023-11-29 18:28:25 -0500

    BugFix and pass data for Barchart as prop

--- frontend/src/components/ColumnChart.jsx
+++ frontend/src/components/ColumnChart.jsx
@@ -4,29 +4,19 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const ColumnChart = ({ totalIncome, totalExpense }) => {
 
-  // Define the data points for the bar chart, with separate values for "income" and "expense"
-  const data = [
-    { category: 'income', income: totalIncome, expense: 0 }, // Data point for income
-    { category: 'expense', income: 0, expense: totalExpense }, // Data point for expense
-  ];
+const ColumnChart = ({ data }) => {
 
   return (
     <Container className="mt-5">
-      <Row className="justify-content-md-center">
-        <Col md={{ span: 6, offset: 3 }}>
-          {/* Display the dropdown for selecting the month (not implemented in the provided code) */}
-        </Col>
-      </Row>
       <Row className="mt-3">
         <Col md={{ span: 6, offset: 3 }}>
           {/* Display the bar chart with shadow and border */}
-          <div className="shadow p-3 mb-5 bg-white rounded" style={{ border: '1px solid #ccc', minHeight: '400px' }}>
+          <div className="shadow p-3 mb-5 bg-white rounded" style={{ border: '1px solid #ccc', width: '400px', height: '400px' }}>
             {/* Create a responsive container for the bar chart */}
             <ResponsiveContainer width="100%" height="100%">
               {/* Define the BarChart component with data and margin */}
-              <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+              <BarChart data={data} >
                 {/* Define the X-axis with the dataKey as "category" */}
                 <XAxis dataKey="category" />
                 {/* Define the Y-axis */}
