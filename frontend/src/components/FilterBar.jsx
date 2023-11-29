12bc9d3a3a8f465cd082d42905f22043df10ac5f
Rora Alem <rora.goitom@gmail.com>
2023-11-29 18:25:15 -0500

    Add handler & call function to update selectedDate

--- frontend/src/components/FilterBar.jsx
+++ frontend/src/components/FilterBar.jsx
@@ -1,50 +1,33 @@
-import moment from 'moment';
+import React from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
+import moment from 'moment';
 
-
-const FilterBar = (props) => {
-  const {
-    date,
-    // setDate,
-    incrementDate,
-    decrementDate
-  } = props;
-
-  // const incrementDate = () => {
-  //   // let currentDate = moment(date);
-  //   let newDate = moment(date).add(1, 'months');
-  //   setDate(newDate);
-  // };
-
-  // const decrementDate = () => {
-  //   // let currentDate = moment(date);
-  //   let newDate = moment(date).add(-1, 'months');
-  //   setDate(newDate);
-  // };
+const FilterBar = ({ date, setDate, updateSelectedMonthAndYear }) => {
+  
+    const handleDateChange = (amount) => {
+      const newDate = moment(date).add(amount, 'months');
+      setDate(newDate);
+      updateSelectedMonthAndYear(newDate);
+    };
 
   return (
-
     <Container className="p-2 mb-4 mt-4 rounded bg-white" style={{ width: "40vw" }}>
-      <Row >
-        <Col className="d-flex justify-content-center" xs={2} onClick={decrementDate} >
+      <Row>
+        <Col className="d-flex justify-content-center" xs={2} onClick={() => handleDateChange(-1)}>
           <ArrowBackIosIcon />
         </Col>
-
-        <Col className="d-flex justify-content-center" xs={8} >
+        <Col className="d-flex justify-content-center" xs={8}>
           <b> {moment(date).format("MMMM YYYY")} </b>
         </Col>
-
-        <Col className="d-flex justify-content-center" xs={2} onClick={incrementDate} >
+        <Col className="d-flex justify-content-center" xs={2} onClick={() => handleDateChange(1)}>
           <ArrowForwardIosIcon />
         </Col>
       </Row>
-
     </Container>
-
   );
 };
 
