320ea20c89f28818314406cbe2a4b7437656e7d3
Tuan Anh Nguyen <atngu312@gmail.com>
2023-11-30 11:44:55 -0500

    add .post to target the correct object from backend

--- backend/src/routes/transfers.js
+++ backend/src/routes/transfers.js
@@ -4,13 +4,13 @@ const transfersQueries = require('../db/queries/transfers_query.js')
 
 router.post('/add', (req, res) => {
   const transferData = {
-    userId: req.body.userId,
-    categoryId: req.body.categoryId,
-    accountFrom: req.body.accountFrom,
-    accountTo: req.body.accountTo,
-    amount: req.body.amount,
-    transaction_date: req.body.transaction_date,
-    notes: req.body.notes
+    userId: 1,
+    categoryId: req.body.post.categoryId,
+    accountId: req.body.post.accountId,
+    accountToId: req.body.post.accountToId,
+    amount: req.body.post.amount,
+    transaction_date: req.body.post.transaction_date,
+    notes: req.body.post.notes
   }
 
   transfersQueries.addTransfer(transferData)
@@ -25,14 +25,14 @@ router.post('/add', (req, res) => {
 
 router.post('/edit', (req, res) => {
   const transferData = {
-    transactionId: req.body.transaction_id,
-    userId: req.body.userId,
-    categoryId: req.body.categoryId,
-    accountFrom: req.body.accountFrom,
-    accountTo: req.body.accountTo,
-    amount: req.body.amount,
-    transaction_date: req.body.transaction_date,
-    notes: req.body.notes
+    transactionId: req.body.post.transaction_id,
+    userId: req.body.post.userId,
+    categoryId: req.body.post.categoryId,
+    accountId: req.body.post.accountId,
+    accountToId: req.body.post.accountToId,
+    amount: req.body.post.amount,
+    transaction_date: req.body.post.transaction_date,
+    notes: req.body.post.notes
   }
 
   transfersQueries.editTransfer(transferData)
