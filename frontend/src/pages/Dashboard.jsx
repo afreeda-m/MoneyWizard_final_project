9ea8d537b645251f93fe66f643506df45a751351
Rora Alem <rora.goitom@gmail.com>
2023-12-01 18:45:38 -0500

    Render PieChart on Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from 'react-bootstrap/Container';
+import axios from "axios";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import { NumericFormat } from "react-number-format";
 import RecentTransactionsList from "../components/RecentTransactionsList";
 import "../styles/Dashboard.scss";
 import MonthlyBalanceChart from "../components/MonthlyBalanceChart";
-
+import PieChartMoneyWizard from "./../components/PieChartMoneyWizard";
+import ColumnChart from "../components/ColumnChart";
 
 const Dashboard = (props) => {
   const { transactionsData,
@@ -19,6 +21,44 @@ const Dashboard = (props) => {
     getCategoryTypeById,
 
   } = props;
+
+  // State for income and expense data
+  const [incomeData, setIncomeData] = useState([]);
+  const [expenseData, setExpenseData] = useState([]);
+
+  // State for income/expense totals
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpense, setTotalExpense] = useState(0);
+  useEffect(() => {
+    axios.get('/transactions/transactionsByCategory')
+      .then(response => {
+        // Transform the response data for use in charts
+
+        const dataForRecharts = response.data.map(item => ({
+          label: item.category_name,
+          value: item.sum,
+          type: item.type,
+        }));
+
+        // Separate income and expense data based on the 'type' property
+        const incomeCategoryList = dataForRecharts.filter(item => item.type === 'Income');
+        const expenseCategoryList = dataForRecharts.filter(item => item.type === 'Expense');
+
+        // Set the filtered data in state
+        setIncomeData(incomeCategoryList);
+        setExpenseData(expenseCategoryList);
+
+        // Calculate and set total income and expense amounts
+        const totalIncomeAmount = incomeCategoryList.reduce((total, item) => total + parseFloat(item.value), 0);
+        const totalExpenseAmount = expenseCategoryList.reduce((total, item) => total + parseFloat(item.value), 0);
+        console.log("total", totalIncomeAmount);
+        setTotalIncome(totalIncomeAmount);
+        setTotalExpense(totalExpenseAmount);
+      })
+      .catch(error => {
+        console.error('Error fetching income and expense distribution data:', error);
+      });
+  }, []);
 
   // calculate total balance for all accounts
   const totalAccountsBalance = accountsData
@@ -45,18 +85,14 @@ const Dashboard = (props) => {
             thousandSeparator={true}
             prefix={"$"}
           />
-        </div>
+</div>
+          {/* Second card showing Income/Expense distribution PieChart */}
+          <div className='pie-card'>
 
-        {/* Second card showing Income/Expense distribution PieChart */}
-        <div className='card shadow p-3 mb-5 bg-white rounded'>
-          <div className='card-inner'>
-            <h3>Income/Expense PieChart</h3>
+            <PieChartMoneyWizard data={incomeData} isExpense={false} />
 
           </div>
-
         </div>
-      </div>
-
       {/* Rendering Charts section */}
       <Row className="justify-content-md-center text-center">
         <Col>
