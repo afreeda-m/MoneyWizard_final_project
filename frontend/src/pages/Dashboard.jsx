afb7779149945fb25ed1a5100657e0a9bb31d223
Rora Alem <rora.goitom@gmail.com>
2023-12-01 17:50:53 -0500

    Render MonthlyBalanceChart on Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/esm/Row";
 import { NumericFormat } from "react-number-format";
 import RecentTransactionsList from "../components/RecentTransactionsList";
 import "../styles/Dashboard.scss";
+import MonthlyBalanceChart from "../components/MonthlyBalanceChart";
 
 
 const Dashboard = (props) => {
@@ -15,7 +16,8 @@ const Dashboard = (props) => {
     getAccountNameById,
     getCategoryIconById,
     getCategoryNameById,
-    getCategoryTypeById
+    getCategoryTypeById,
+
   } = props;
 
   // calculate total balance for all accounts
@@ -23,7 +25,7 @@ const Dashboard = (props) => {
     .map((account) => account.balance)
     .reduce((a, b) => a + b, 0);
 
-    // Get the top 5 recent transactions
+  // Get the top 5 recent transactions
   const top5RecentTransactions = transactionsData.slice(0, 5);
 
   return (
@@ -31,7 +33,7 @@ const Dashboard = (props) => {
       <h1>Dashboard</h1>
       <div className='main-cards'>
 
-           {/* First card for total balance */}
+        {/* First card for total balance */}
         <div className='card shadow p-3 mb-5 bg-white rounded'>
           <div className='card-inner'>
             <h3>TOTAL BALANCE</h3>
@@ -51,7 +53,7 @@ const Dashboard = (props) => {
             <h3>Income/Expense PieChart</h3>
 
           </div>
-          <h1>{" "}</h1>
+
         </div>
       </div>
 
@@ -59,13 +61,13 @@ const Dashboard = (props) => {
       <Row className="justify-content-md-center text-center">
         <Col>
           <div className="shadow p-3 mb-5 bg-white rounded" style={{ width: "50vw" }}>
-            <h1>Barchart </h1>
-            <p> Monthly balance chart </p>
+
+            <MonthlyBalanceChart />
           </div>
         </Col>
       </Row>
 
-       {/* Rendering Recent Transactions section */}
+      {/* Rendering Recent Transactions section */}
       <Row className="justify-content-md-center text-center">
         <Col >
           <div className="shadow p-3 mb-5 bg-white rounded ">
@@ -86,7 +88,7 @@ const Dashboard = (props) => {
         </Col>
       </Row>
 
- </Container>
+    </Container>
 
   );
 };
