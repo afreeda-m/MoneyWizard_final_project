86d1843fbc67dfb606c0436d8865c91a0aaa0ed2
jvarghe <vcs.inbox@outlook.com>
2023-12-01 06:40:06 -0500

    Fill out 'CategoriesManagement.jsx' component; add variables, useEffect(), and return statement.

--- frontend/src/pages/CategoriesManagement.jsx
+++ frontend/src/pages/CategoriesManagement.jsx
@@ -1,14 +1,82 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import CategoriesList from "../components/CategoriesList";
+import FloatingActionButton from "../components/FloatingActionButton";
 
 
 const CategoriesManagement = () => {
-  return (
-    <div className="categoriesManagement  d-flex flex-column align-items-center">
 
-      <h1>Categories Management Page</h1>
+  const [incomeCategories, setIncomeCategories] = useState([]);
+  const [expenseCategories, setExpenseCategories] = useState([]);
+
+
+  // When the page loads for the first time...
+  useEffect(() => {
+
+    const incomeCategoryUrl = "/category/type/Income";
+    const expenseCategoryUrl = "/category/type/Expense";
+
+
+    // ...Category names needs to be retrieved from the backend twice: first,
+    // for Income-related categories and secondly, for Expense-related
+    // categories. This is a generic function that makes call to the same
+    // API route, using different parameters (`Income` or `Expense`). Note that
+    // both parameter names are case sensitive. (Note: The database also
+    // contains a third type: `Transfer`).
+    const retrieveCategoryData = function(url) {
+
+      axios.get(url)
+
+        .then((response) => {
+          // console.log(response.data);
+
+          if (url === "/category/type/Income") {
+            setIncomeCategories(response.data);
+          } else {
+            setExpenseCategories(response.data);
+          }
+
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+
+    };
+
+
+    // Call this method twice to populate both Income & Expense data sets.
+    retrieveCategoryData(incomeCategoryUrl);
+    retrieveCategoryData(expenseCategoryUrl);
+
+  }, []);
+
+
+  // Test if the calls to the backend API went through properly.
+  // console.log(incomeCategories);
+  // console.log(expenseCategories);
+
+
+  return (
+
+    <div className="categoriesManagement d-flex flex-column align-items-center">
+
+      <div class="d-flex flex-column align-items-center bg-body-tertiary mt-5">
+
+        <h1>Categories Management</h1>
+
+        <CategoriesList
+          incomeCategories={incomeCategories}
+          expenseCategories={expenseCategories}
+        />
+
+        <FloatingActionButton />
+
+      </div>
 
     </div>
+
   );
+
 };
 
 export default CategoriesManagement;
