e3c0e3dfe746afd00096db2ff5960bf7cddc7fbb
Rora Alem <rora.goitom@gmail.com>
2023-12-01 08:50:10 -0500

    Render RecentTransactionsList in Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import { NumericFormat } from "react-number-format";
-import TransactionList from "../components/TransactionList";
+import RecentTransactionsList from "../components/RecentTransactionsList";
 import "../styles/Dashboard.scss";
 
 
@@ -18,19 +18,23 @@ const Dashboard = (props) => {
     getCategoryTypeById
   } = props;
 
-
+  // calculate total balance for all accounts
   const totalAccountsBalance = accountsData
     .map((account) => account.balance)
     .reduce((a, b) => a + b, 0);
+
+    // Get the top 5 recent transactions
+  const top5RecentTransactions = transactionsData.slice(0, 5);
 
   return (
     <Container className="mt-5 d-flex flex-column align-items-center bg-body-tertiary">
       <h1>Dashboard</h1>
       <div className='main-cards'>
+
+           {/* First card for total balance */}
         <div className='card shadow p-3 mb-5 bg-white rounded'>
           <div className='card-inner'>
             <h3>TOTAL BALANCE</h3>
-
           </div>
           <h1>{" "}</h1>
           <NumericFormat
@@ -39,9 +43,9 @@ const Dashboard = (props) => {
             thousandSeparator={true}
             prefix={"$"}
           />
-
-
         </div>
+
+        {/* Second card showing Income/Expense distribution PieChart */}
         <div className='card shadow p-3 mb-5 bg-white rounded'>
           <div className='card-inner'>
             <h3>Income/Expense PieChart</h3>
@@ -49,35 +53,26 @@ const Dashboard = (props) => {
           </div>
           <h1>{" "}</h1>
         </div>
-
-
       </div>
 
+      {/* Rendering Charts section */}
       <Row className="justify-content-md-center text-center">
-
         <Col>
           <div className="shadow p-3 mb-5 bg-white rounded" style={{ width: "50vw" }}>
             <h1>Barchart </h1>
             <p> Monthly balance chart </p>
-
-
           </div>
         </Col>
-        {/* <Col  >
-          <div className="shadow p-3 mb-5 bg-white rounded " style={{ border: '1px solid #ccc', width: '400px', height: '200px' }}>
-            <h1 >Line chart </h1>
-            <p> Monthly balance chart </p>
-          </div>
-        </Col> */}
       </Row>
+
+       {/* Rendering Recent Transactions section */}
       <Row className="justify-content-md-center text-center">
-
-
-        <Col  >
+        <Col >
           <div className="shadow p-3 mb-5 bg-white rounded ">
-            <h1 >Recent Transactions </h1>
-            <TransactionList
-              transactionsData={transactionsData}
+
+            <h1>Recent Transactions </h1>
+            < RecentTransactionsList
+              transactionsData={top5RecentTransactions}
               categoriesData={categoriesData}
               accountsData={accountsData}
               getAccountNameById={getAccountNameById}
@@ -91,8 +86,7 @@ const Dashboard = (props) => {
         </Col>
       </Row>
 
-
-    </Container>
+ </Container>
 
   );
 };
