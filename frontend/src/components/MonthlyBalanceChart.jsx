77f5a89ba5bb1cd3e1b8c9c687cd40a7dee6449c
Rora Alem <rora.goitom@gmail.com>
2023-12-01 17:49:51 -0500

    Add monthly balance chart component

---
+++ frontend/src/components/MonthlyBalanceChart.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { BarChart, Bar, Label, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import moment from 'moment';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+
+const MonthlyBalanceChart = () => {
+  const [transactionsData, setTransactionsData] = useState([]);
+  const [monthlyData, setMonthlyData] = useState([]);
+
+  useEffect(() => {
+
+    // Fetch monthly transactions when the component mounts
+    const userId = 1; // Replace with logic to get user ID from cookies or elsewhere
+    const year = moment().format('YYYY');
+    const month = moment().format('MM');
+
+    axios
+      .get(`transactions/monthlyTransactions`, {
+        params: { year, month, userId },
+      })
+      .then((response) => {
+        setTransactionsData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching monthly transactions data:', error);
+      });
+
+    // Process transactionsData when it changes
+    const dataForRecharts = transactionsData.map((item) => ({
+      month: moment(item.transaction_date).format('MMM YYYY'),
+      balance: item.amount,
+    }));
+
+    const aggregatedData = dataForRecharts.reduce((acc, item) => {
+      const key = item.month;
+      if (!acc[key]) {
+        acc[key] = { month: key, balance: 0 };
+      }
+      acc[key].balance += item.balance;
+      return acc;
+    }, {});
+
+    const resultData = Object.values(aggregatedData);
+
+    setMonthlyData(resultData);
+  }, [transactionsData]);
+
+  // Custom Y-axis tick component for formatting
+  const CustomYAxisTick = ({ x, y, payload }) => (
+    <g transform={`translate(${x},${y})`}>
+      <text x={0} y={0} dy={16} textAnchor="end" fill="#666">
+        ${payload.value}
+      </text>
+    </g>
+  );
+
+  // Custom tooltip component for formatting
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`${label} : $${payload[0].value}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <p className="monthly-balance-chart-title">Monthly Balance Chart</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart data={monthlyData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month">
+                <Label value="Month" position="insideBottom" offset={-10} />
+              </XAxis>
+              <YAxis tickFormatter={(value) => `$${value}`} />
+              <Tooltip content={<CustomTooltip />} />
+              <Bar dataKey="balance" fill="#82ca9d" name="Monthly Balance" />
+            </BarChart>
+          </ResponsiveContainer>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+
+export default MonthlyBalanceChart;
