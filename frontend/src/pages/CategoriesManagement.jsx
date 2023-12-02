a838f56a64dac36d4765e8f511c8c32e7de3f68e
Tuan Anh Nguyen <atngu312@gmail.com>
2023-12-02 01:27:05 -0500

    delete unused codes, add comments and edit styling for Category page

--- frontend/src/pages/CategoriesManagement.jsx
+++ frontend/src/pages/CategoriesManagement.jsx
@@ -1,6 +1,5 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
 import CategoriesList from "../components/CategoriesList";
+import CategoriesModalAddNew from "../components/CategoriesModalAddNew";
 import FloatingActionButton from "../components/FloatingActionButton";
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
@@ -8,74 +7,50 @@ import Tabs from 'react-bootstrap/Tabs';
 
 const CategoriesManagement = (props) => {
 
-  const { categoriesData, getCategories } = props;
+  const {
+    categoriesData,
+    getCategories,
+    isAddCategoryModalOpen,
+    toggleAddCategoryModal
+  } = props;
 
   const incomeCategories = categoriesData.filter((category) => category.type === "Income");
   const expenseCategories = categoriesData.filter((category) => category.type === "Expense");
 
-
-
-  // This function deletes a Category from a user's account.
-  // const deleteCategory = function(categoryId, categoryType) {
-
-  //   let categoryUrl;
-
-  //   if (categoryType === "Income") {
-  //     categoryUrl = incomeCategoryUrl;
-  //   } else {
-  //     categoryUrl = expenseCategoryUrl;
-  //   }
-
-  //   console.log(categoryId, categoryType);
-
-
-  //   axios.post(`/categories/${categoryId}/delete`)
-  //     .then((response) => {
-  //       console.log(response.data.message);
-  //     }).then(() => {
-  //       if (categoryType === "Income") {
-  //         setIncomeCategories(retrieveCategoryData(categoryUrl));
-  //       }
-  //       if (categoryType === "Expense") {
-  //         setExpenseCategories(retrieveCategoryData(categoryUrl));
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error Deleting Category: ", error);
-  //     });
-
-  // };
-
-
   return (
 
-    <div className="categoriesManagement d-flex flex-column align-items-center">
+    <div className="d-flex flex-column align-items-center bg-body-tertiary mt-5">
 
-      <div class="d-flex flex-column align-items-center bg-body-tertiary mt-5">
+      <h1 className='mb-5'>Categories Management</h1>
 
-        {/* <h1>Categories Management</h1> */}
-        <Tabs defaultActiveKey="income" transition={false} justify style={{ width: "30vw" }}>
+      {/* Render category list with React Bootstrap Tabs component */}
+      <Tabs defaultActiveKey="income" transition={false} justify style={{ width: "30vw" }}>
 
-          <Tab eventKey="income" title="INCOME">
-            <CategoriesList
-              categories={incomeCategories}
-              getCategories={getCategories}
-            />
-          </Tab >
+        <Tab eventKey="income" title="INCOME">
+          <CategoriesList
+            categories={incomeCategories}
+            getCategories={getCategories}
+          />
+        </Tab >
 
-          <Tab eventKey="expense" title="EXPENSE">
-            <CategoriesList
-              categories={expenseCategories}
-              getCategories={getCategories}
-            />
-          </Tab>
+        <Tab eventKey="expense" title="EXPENSE">
+          <CategoriesList
+            categories={expenseCategories}
+            getCategories={getCategories}
+          />
+        </Tab>
 
-        </Tabs>
+      </Tabs>
 
-
+      {/* Floating button to toggle CategoryModalAddNew */}
+      <div onClick={toggleAddCategoryModal}>
         <FloatingActionButton />
-
       </div>
+
+      <CategoriesModalAddNew
+        isAddCategoryModalOpen={isAddCategoryModalOpen}
+        toggleAddCategoryModal={toggleAddCategoryModal}
+      />
 
     </div>
 
