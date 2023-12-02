43808468c3071542f9879a3e0eb20804f94782f6
jvarghe <vcs.inbox@outlook.com>
2023-12-01 21:59:43 -0500

    Add the 'CategoriesModalAddNew' component.

---
+++ frontend/src/components/CategoriesModalAddNew.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+
+function CategoriesModalAddNew() {
+
+  return (
+    <div>CategoriesModalAddNew</div>
+  );
+
+}
+
+export default CategoriesModalAddNew;
