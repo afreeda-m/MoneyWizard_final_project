84804977eab4269577e86132e482ee59ac014746
Tuan Anh Nguyen <atngu312@gmail.com>
2023-12-03 18:31:51 -0500

    implement icon selection feature

--- frontend/src/components/CategoriesModalAddNew.jsx
+++ frontend/src/components/CategoriesModalAddNew.jsx
@@ -3,6 +3,7 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 
 function CategoriesModalAddNew(props) {
@@ -12,13 +13,20 @@ function CategoriesModalAddNew(props) {
     toggleAddCategoryModal,
     postCategoryData,
     setPostCategoryData,
-    getCategories
+    getCategories,
+    iconsData
   } = props;
 
+
+  // EVENT HANDLING FUNCTIONS
   // Update postCategoryData state upon user input on the form
   const handleInput = (event) => {
-    console.log(postCategoryData);
     setPostCategoryData({ ...postCategoryData, [event.target.name]: event.target.value });
+  };
+
+  // Update logo_url in postCategoryData state upon selection in the drop down list for icon
+  const handleIconSelect = (eventKey) => {
+    setPostCategoryData({ ...postCategoryData, logo_url: eventKey });
   };
 
   // Function to handle all events upon closing the Modal
@@ -34,6 +42,7 @@ function CategoriesModalAddNew(props) {
     toggleAddCategoryModal();
   };
 
+  // Function to handle form submission
   const handleSubmit = (event) => {
 
     event.preventDefault();
@@ -49,10 +58,24 @@ function CategoriesModalAddNew(props) {
         console.error("Error posting new category:", error);
       });
 
+    // Reset the postCategoryData state to default
+    setPostCategoryData({
+      category_name: null,
+      type: null,
+      logo_url: null,
+      user_id: null
+    });
     // Close the Modal
     toggleAddCategoryModal();
   };
 
+  const listOfIcons = iconsData.map((icon) => {
+    return (
+      <Dropdown.Item key={icon.id} eventKey={icon.logo_url}>
+        <img src={`/images/${icon.logo_url}`} alt={icon.logo_url} />
+      </Dropdown.Item>
+    );
+  });
 
 
   return (
@@ -60,7 +83,7 @@ function CategoriesModalAddNew(props) {
       style={{ marginLeft: "130px" }}
       show={isAddCategoryModalOpen}
       onHide={handleClose}
-      size="sm"
+      size="md"
       centered
     >
 
@@ -68,13 +91,12 @@ function CategoriesModalAddNew(props) {
         <Modal.Title>ADD CATEGORY</Modal.Title>
       </Modal.Header>
 
+
       <Modal.Body>
 
         <Form.Group className='mb-3'>
-
           <Form.Label >Category name</Form.Label>
           <Form.Control type='text' name="category_name" onChange={handleInput} />
-
         </Form.Group>
 
         <Form.Group className='mb-3'>
@@ -87,21 +109,37 @@ function CategoriesModalAddNew(props) {
         </Form.Group>
 
 
-        {/* TO BE REVIEWED: Need to find a way to display the bank of images and set the value to the image path on user selection */}
         <Form.Group>
           <Form.Label>Category Icon</Form.Label>
-          <Form.Control name="logo_url" onChange={handleInput}></Form.Control>
+
+          <Dropdown onSelect={handleIconSelect}>
+            {/* Remove background color with variant "none" and remove down arrow icon by setting id */}
+            <Dropdown.Toggle variant="none" id="dropdown-basic" >
+              {/* Display a blank blue icon upon modal opening and change into the selected one after selection */}
+              {
+                postCategoryData.logo_url === null
+                  ?
+                  <img src='/images/blue.png' alt='icon selection' />
+                  :
+                  <img src={`/images/${postCategoryData.logo_url}`} alt="selected icon" />
+              }
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ width: "100px", height: "400px", overflow: "scroll" }}>
+              {listOfIcons}
+            </Dropdown.Menu>
+          </Dropdown>
         </Form.Group>
 
       </Modal.Body>
 
-      <Modal.Footer className='d-flex justify-content-around mb-2'>
 
+      <Modal.Footer className='d-flex justify-content-around mb-2'>
         <Button type="button" variant="secondary" onClick={handleClose}>
           Close
         </Button>
 
-        <Button type="submit" variant="primary" onClick={handleSubmit}>
+        <Button type="submit" variant="success" onClick={handleSubmit}>
           Save
         </Button>
       </Modal.Footer>
