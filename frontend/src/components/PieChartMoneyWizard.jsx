cc98274ef104ce439e7d2b9e82904377e4a7cb0d
Rora Alem <rora.goitom@gmail.com>
2023-12-02 21:16:53 -0500

    Display a message when there's no data for chart

--- frontend/src/components/PieChartMoneyWizard.jsx
+++ frontend/src/components/PieChartMoneyWizard.jsx
@@ -4,7 +4,23 @@ import { PieChart, Pie, Cell, Tooltip, Label, Legend } from "recharts";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const PieChartComponent = ({ data, isExpense }) => {
+const PieChartComponent = ({ data, isExpense}) => {
+
+  // Check if there is no data
+  const hasData = data.length > 0;
+
+  if (!hasData) {
+    // Display a message or notification when there is no data
+    return (
+      <Container className='mt-5'>
+        <Row className="shadow p-3 mb-5 bg-white rounded" style={{ border: '1px solid #ccc' }}>
+          <Col className='justify-content-md-center'>
+            <p>No {isExpense ? 'expense' : 'income'} data available for charts.</p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
   // Calculate total for percentage calculation
   const total = data.reduce((total, item) => total + parseFloat(item.value), 0);
 
@@ -17,57 +33,79 @@ const PieChartComponent = ({ data, isExpense }) => {
   }));
 
   // Define fixed colors for the chart segments
-  const colors = ['#FF8080', '#80C7FF', '#A0FFA0', '#FFD080', '#D080FF'];
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', "#FFD300", "#8f2d56", "#BE0AFF", "#f15bb5"];
   const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-    index,
-  }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+  payload, // Add payload to access entry data
+}) => {
+  const RADIAN = Math.PI / 180;
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-    return (
+  const lineStartX = cx + (outerRadius + 10) * Math.cos(-midAngle * RADIAN);
+  const lineStartY = cy + (outerRadius + 10) * Math.sin(-midAngle * RADIAN);
+  const lineEndX = cx + (outerRadius + 30) * Math.cos(-midAngle * RADIAN);
+  const lineEndY = cy + (outerRadius + 30) * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <>
+      {/* Line */}
+      <line
+        x1={lineStartX}
+        y1={lineStartY}
+        x2={lineEndX}
+        y2={lineEndY}
+        stroke="black"
+        strokeWidth={1}
+      />
+      {/* Label text */}
       <text
-        x={x}
-        y={y}
-        fill="white"
-        textAnchor={x > cx ? "start" : "end"}
+        x={lineEndX}
+        y={lineEndY}
+        fill="black"
+        textAnchor={lineEndX > cx ? "start" : "end"}
         dominantBaseline="central"
       >
-        {`${(percent * 100).toFixed(0)}%`}
+        {`${payload.label} ${(percent * 100).toFixed(0)}%`}
       </text>
-    );
-  };
+    </>
+  );
+};
 
   return (
     <Container className='mt-5'>
-      <Row className="shadow p-3 mb-5 bg-white rounded" style={{ border: '1px solid #ccc' }}>
+      <Row>
         <Col className='justify-content-md-center'>
-          <PieChart width={400} height={400}>
+          <PieChart width={600} height={400}>
             <Pie
               dataKey="percentage"
               nameKey="label"
               data={dataWithPercentage}
               cx="50%"
               cy="50%"
-              outerRadius={130}
+              outerRadius={"80%"}
               fill="#8884d8"
               label={renderCustomizedLabel}
-              labelLine={false}
-              innerRadius={isExpense ? 0 : 40} // Customize innerRadius based on isExpense
+
+               // Customize innerRadius based on isExpense
             >
               {dataWithPercentage.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
               ))}
               <Label layout="vertical" align="right" verticalAlign="middle" />
             </Pie>
-            <Tooltip formatter={(value) => `${value.toFixed()}%`} />
+            <Tooltip
+            formatter={(value) => `${value.toFixed()}%`}
+            contentStyle={{background: "transparent", border:"none"}}
+            labelStyle={{display:"none"}}/>
             <Legend />
           </PieChart>
         </Col>
