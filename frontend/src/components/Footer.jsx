5070c66aaf6166b80bcfeddcbdad96f4fb8619d2
jvarghe <vcs.inbox@outlook.com>
2023-12-05 16:20:14 -0500

    Change 'Footer.jsx' background and text colour.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
 
     // `bg-body-tertiary`: Give its the same colour as the NavBar.
-    <footer className='navbar-expand navbar-light bg-body-tertiary' style={{marginTop: "auto"}}>
+    <footer className='navbar-expand bg-primary' style={{marginTop: "auto"}}>
 
       <Container>
 
@@ -18,15 +18,15 @@ function Footer() {
           */}
         <div className='text-center py-3'>
 
-          <p> Copyright &copy; 2023: &nbsp;
+          <p className='text-light'> Copyright &copy; 2023:&nbsp;
 
-            <a className='text-dark' href='https://github.com/afreeda-m/MoneyWizard_final_project'>
+            <a className='text-light' href='https://github.com/afreeda-m/MoneyWizard_final_project'>
               The Wizards of Money Wizard
             </a>
 
           </p>
 
-          <p>All Rights Reserved</p>
+          <p className='text-light'>All Rights Reserved</p>
 
         </div>
       </Container>
