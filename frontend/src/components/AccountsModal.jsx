cb22a0548c9ce7ba374111d5abe1dcfe056f270d
Tuan Anh Nguyen <atngu312@gmail.com>
2023-12-07 10:15:44 -0500

    edit syntax

--- frontend/src/components/AccountsModal.jsx
+++ frontend/src/components/AccountsModal.jsx
@@ -44,30 +44,30 @@ function AccountsModal(props) {
         <Modal.Body>
           <form id="account-form" onSubmit={submitForm}>
 
-            <div class="form-group">
-              <label for="recipient-name" class="col-form-label">
+            <div className="form-group">
+              <label htmlFor="recipient-name" className="col-form-label">
                 Account Name
               </label>
-              <input type="text" name="accountName" class="form-control" id="account-name" />
+              <input type="text" name="accountName" className="form-control" id="account-name" />
             </div>
 
-            <div class="form-group">
-              <label for="message-text" class="col-form-label">
+            <div className="form-group">
+              <label htmlFor="message-text" className="col-form-label">
                 Balance
               </label>
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">$</span>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">$</span>
                 </div>
-                <input class="form-control" name="balance" id="account-balance" />
+                <input className="form-control" name="balance" id="account-balance" />
               </div>
             </div>
 
-            <div class="form-group">
-              <label for="message-text" class="col-form-label">
+            <div className="form-group">
+              <label htmlFor="message-text" className="col-form-label">
                 Notes
               </label>
-              <input class="form-control" name="notes" id="account-notes" />
+              <input className="form-control" name="notes" id="account-notes" />
             </div>
 
           </form>
