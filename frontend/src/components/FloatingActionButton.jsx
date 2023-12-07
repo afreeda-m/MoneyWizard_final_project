5c5c9b005ee2925f59cb5abc1851ccea40956d23
Tuan Anh Nguyen <atngu312@gmail.com>
2023-12-06 22:04:19 -0500

    reposition floating button

--- frontend/src/components/FloatingActionButton.jsx
+++ frontend/src/components/FloatingActionButton.jsx
@@ -27,7 +27,7 @@ function FloatingActionButton(props) {
         {/* THE FLOATING ACTION BUTTON COMPONENT */}
         <Fab color="success"
           aria-label="add"
-          style={{ position: "fixed", bottom: "9rem", right: "3rem" }}
+          style={{ position: "fixed", bottom: "8rem", right: "3rem" }}
           onClick={props.click}
         >
 
