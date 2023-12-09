1740fb1120143d409b4739222943323c11b82304
Tuan Anh Nguyen <atngu312@gmail.com>
2023-12-09 00:51:29 -0500

    create getAccountById and getCategoryById

--- frontend/src/helpers/helperFunctions.js
+++ frontend/src/helpers/helperFunctions.js
@@ -3,27 +3,36 @@ import transactions from "../mocks/transactions";
 
 export const getCategoryNameById = (id, categories) => {
   const result = categories.find(category => category.id === id);
-
   return result.category_name;
 };
 
 export const getCategoryIconById = (id, categories) => {
   const result = categories.find(category => category.id === id);
-
   return result.logo_url;
 };
 
 export const getCategoryTypeById = (id, categories) => {
   const result = categories.find(category => category.id === id);
-
   return result.type;
 };
 
 export const getAccountNameById = (id, accounts) => {
   const result = accounts.find(account => account.id === id);
-
   return result.account_name;
 };
+
+export const getCategoryById = (id, categories) => {
+  const result = categories.find(category => category.id === id);
+  return result;
+};
+
+export const getAccountById = (id, accounts) => {
+  const result = accounts.find(account => account.id === id);
+  return result;
+};
+
+
+
 // Calculate income distribution by category
 export const calculateIncomeDistribution = () => {
   const totalIncome = transactions
